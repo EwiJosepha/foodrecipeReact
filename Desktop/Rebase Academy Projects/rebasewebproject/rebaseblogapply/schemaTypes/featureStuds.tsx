1c859054e32395a47991c6ce6d597c38ad3fb49c
ewi josepha <ewijosepha@gmail.com>
2024-07-02 20:42:11 +0200

    adding document for testimonies

---
+++ Desktop/Rebase Academy Projects/rebasewebproject/rebaseblogapply/schemaTypes/featureStuds.tsx
@@ -0,0 +1,31 @@
+
+const FeatStudents  =  {
+  name: 'featStudents',
+  title: 'FeatStudents',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: "studImage",
+      type: "image",
+      title: "Stud Image"
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Description"
+    },
+    {
+      name: 'testimony',
+      title: 'Testimony',
+      type: 'text',
+      rows: 5, // Adjust the number of rows for the textarea as needed
+    },
+  ],
+};
+
+export default FeatStudents
---
+++ Desktop/Rebase Academy Projects/rebasewebproject/rebaseblogapply/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import {applySchema}  from "./applySchema"
+import blogsSchema from "./blogs"
+import contact from "./contact-content"
+import FeatStudents from "./featureStuds"
+export const schemaTypes = [blogsSchema, contact, applySchema, FeatStudents]
